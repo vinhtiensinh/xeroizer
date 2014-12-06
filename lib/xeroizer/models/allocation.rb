42dd6c8eeabe73a27b8710ef25f2b623691e8082
Vinh Tran <qvinh.tran@gmail.com>
2014-12-06 16:11:35 +1100

    update so that we can save allocation individually

--- lib/xeroizer/models/allocation.rb
+++ lib/xeroizer/models/allocation.rb
@@ -1,13 +1,28 @@
 module Xeroizer
   module Record
     class AllocationModel < BaseModel
+      attr_accessor :credit_note
+
+      set_api_controller_name 'CreditNotes'
+
+      def url
+        "#{super}/#{credit_note.id}/Allocations"
+      end
     end
 
     class Allocation < Base
+      attr_accessor :credit_note
+      set_primary_key :id
+
+      guid    :id
       decimal :applied_amount
       belongs_to :invoice
-
       validates_presence_of :invoice
+
+      def save
+        self.parent.credit_note = self.credit_note
+        super
+      end
     end
   end
 end
