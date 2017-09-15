7b284d7eabeb27e6654349c4fc76e3e18acae8bc
Vinh Tran <qvinh.tran@gmail.com>
2017-09-15 14:02:17 +1000

    fix xero authorization

--- lib/xeroizer/partner_application.rb
+++ lib/xeroizer/partner_application.rb
@@ -19,8 +19,8 @@ module Xeroizer
       # @return [PartnerApplication] instance of PrivateApplication
       def initialize(consumer_key, consumer_secret, path_to_private_key, path_to_ssl_client_cert, path_to_ssl_client_key, options = {})
         default_options = {
-          :xero_url         => 'https://api-partner.network.xero.com/api.xro/2.0',
-          :site             => 'https://api-partner.network.xero.com',
+          :xero_url         => 'https://api.xero.com/api.xro/2.0',
+          :site             => 'https://api.xero.com',
           :authorize_url    => 'https://api.xero.com/oauth/Authorize',      
           :signature_method => 'RSA-SHA1'
         }
